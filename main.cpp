2727bee92701e90aa41e44b54ae1bc12b2fb2496
Soo-Hyun Yoo <yoos117@gmail.com>
2013-08-16 00:15:28 -0700

    Move tests out of main.

--- main.cpp
+++ main.cpp
@@ -2,21 +2,12 @@
 #include <stdlib.h>
 #include <fstream>
 
+#include <test.h>
 #include <hashmap.h>
 
 int main(int argc, char **argv)
 {
-	HashLink hello("hello", 1);
-	HashLink world("world", 2);
-
-	hello.next(&world);
-
-	printf("%d %d\n", hello.val(), hello.next()->val());
-
-	hello.val(3);
-	hello.next()->val(5);
-
-	printf("%d %d\n", hello.val(), hello.next()->val());
+	test_all();
 
 	return 0;
 }
