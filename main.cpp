68ad305e380ccda4e862a60f330f928a02ac2fc9
Soo-Hyun Yoo <yoos117@gmail.com>
2013-08-15 23:00:41 -0700

    Test HashLink.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 #include <fstream>
 
+#include <hashmap.h>
+
 int main(int argc, char **argv)
 {
+	HashLink hello("hello", 1);
+	HashLink world("world", 2);
+
+	hello.next(&world);
+
+	printf("%d %d\n", hello.val(), hello.next()->val());
+
+	hello.val(3);
+	hello.next()->val(5);
+
+	printf("%d %d\n", hello.val(), hello.next()->val());
 
 	return 0;
 }
