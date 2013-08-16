e6d841f6cd31e077b0d068cd73312d596d5ff958
Soo-Hyun Yoo <yoos117@gmail.com>
2013-08-16 02:08:04 -0700

    Implement the map part of hashmap.

--- hashmap.cpp
+++ hashmap.cpp
@@ -32,3 +32,121 @@ void HashLink::next(HashLink* p)
 	_next = p;
 }
 
+
+HashMap::HashMap(uint64_t tableSize)
+{
+	_table = (HashLink**) calloc(tableSize, sizeof(HashLink*));
+	_tableSize = tableSize;
+	_count = 0;
+}
+
+HashMap::~HashMap()
+{
+	// Deallocate all HashLinks and table.
+	for (uint64_t i=0; i<_tableSize; i++) {
+		if (_table[i] != 0) {
+			_deleteHashLink(_table[i]);
+		}
+	}
+}
+
+VALTYPE HashMap::get(KEYTYPE key)
+{
+	uint64_t hash = _hashKey(key);
+	HashLink* link = _table[hash];
+
+	// Find the link with the right key, assuming that if a bucket exists here, the key has to be here, too.
+	if (link != 0) {
+		while (link->key() != key) {
+			link = link->next();
+		}
+		return link->val();
+	}
+
+	// Otherwise, return 0.
+	return 0;
+}
+
+void HashMap::inc(KEYTYPE key)
+{
+	uint64_t hash = _hashKey(key);
+	HashLink* link = _table[hash];
+
+	if (link != 0) {
+		if (link->key() == key) {
+			link->val(link->val()+1);
+		}
+		else if (link->next() == 0) {
+			link->next(new HashLink(key, 1));
+			_count++;
+		}
+		else {
+			link = link->next();
+		}
+	}
+	else {
+		_table[hash] = new HashLink(key, 1);
+		_count++;
+	}
+}
+
+uint64_t HashMap::getCount()
+{
+	return _count;
+}
+
+uint64_t HashMap::getCapacity()
+{
+	return _tableSize;
+}
+
+void HashMap::getKeys(KEYTYPE* keys)
+{
+	uint64_t key_index = 0;
+	HashLink* hl = 0;   // Keep track of next HashLink from which to grab a key.
+
+	for (uint64_t i=0; i<_tableSize; i++) {
+		hl = _table[i];
+		while (hl != 0) {
+			keys[key_index] = hl->key();
+			hl = hl->next();
+		}
+	}
+}
+
+uint64_t HashMap::_hashKey(KEYTYPE key)
+{
+	uint64_t out = 0;
+	for (int i=0; key[i] != '\0'; i++) {
+		out += key[i];
+	}
+	return out;
+}
+
+void HashMap::_deleteHashLink(HashLink* p)
+{
+	if (p->next() != 0) {
+		_deleteHashLink(p->next());
+	}
+	delete p;
+}
+
+void HashMap::_insert(KEYTYPE key, VALTYPE val)
+{
+	uint64_t hash = _hashKey(key);
+	HashLink* link = _table[hash];
+
+	// If bucket already exists, find the last link in bucket.
+	if (link != 0) {
+		while (link->next() != 0) {
+			link = link->next();
+		}
+		link->next(new HashLink(key, val));
+	}
+	else {
+		_table[hash] = new HashLink(key, val);
+	}
+
+	_count++;
+}
+
--- hashmap.h
+++ hashmap.h
@@ -1,6 +1,11 @@
+/**
+ * Minimal hashmap implementation for this task.
+ */
+
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <string>
 
@@ -8,7 +13,7 @@
 #define VALTYPE uint16_t
 
 class HashLink {
-	KEYTYPE _key;
+	KEYTYPE     _key;
 	VALTYPE     _val;
 	HashLink*   _next;
 
@@ -20,6 +25,43 @@ public:
 	void      val(VALTYPE v);      // Set value
 	void      next(HashLink* p);   // Set next link
 };
+
+class HashMap {
+	HashLink** _table;        // Array of pointers to HashLinks
+	uint64_t   _tableSize;    // Size of table
+	uint64_t   _count;        // Number of HashLinks
+
+	// Helper functions
+	uint64_t _hashKey(KEYTYPE key);   // Generate hash.
+	void _deleteHashLink(HashLink* p);   // Delete HashLinks.
+	void _insert(KEYTYPE key, VALTYPE val);   // Insert key-value pairs.
+
+public:
+	HashMap(uint64_t tableSize);
+	~HashMap();
+
+	VALTYPE get(KEYTYPE key);   // Get value
+	void    inc(KEYTYPE key);   // Increment.
+
+	/**
+	 * @brief Get number of links in table.
+	 */
+	uint64_t getCount();
+
+	/**
+	 * @brief Get capacity of table.
+	 */
+	uint64_t getCapacity();
+
+	/**
+	 * @brief Store all keys in array allocated by caller.
+	 * @param keys Pointer to array storing keys.
+	 *
+	 * @output Number of keys stored in array.
+	 */
+	void getKeys(KEYTYPE* keys);
+};
+
 
 #endif // HASHMAP_H
 
