472ce841408419d3c46bc5ec70055c6171c31a17
Soo-Hyun Yoo <yoos117@gmail.com>
2013-08-17 01:45:24 -0700

    Add config file.

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Performance
+#define HASHMAP_INIT_CAPACITY 100   // Initial capacity of hashmap used to store words found in input text file.
+#define WORD_MAXLENGTH 200
+
+// HashMap
+#define KEYTYPE std::string
+#define VALTYPE uint16_t
+#define LOAD_THRESHOLD 0.75   // Table load threshold at which we increase table size.
+#define RESIZE_FACTOR 2   // Multiply table size by this factor on resize.
+
+#endif // CONFIG_H
+
